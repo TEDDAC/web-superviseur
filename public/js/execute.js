8e332846d2e93a87eb29359d49c86f926459b3d1
Mathis RIBEMONT <m.ribemont@w3plus.fr>
2022-07-05 11:29:04 +0200

    correction bug date et défaut

--- public/js/execute.js
+++ public/js/execute.js
@@ -3,13 +3,13 @@ async function runTab(array) {
     return;
   }
   array.forEach(element => {
-    if(element.nextDateTimeTriggered === undefined || element.nextDateTimeTriggered === null){
-      element.nextDateTimeTriggered == new Date(Date.now() + element.frequence*60*1000);
-    }
+      element.nextDateTimeTriggered = Date.now() + element.frequence*60*1000;
   });
   try {
     let tab = window.open();
     let i = 0;
+    console.log(array);
+    console.log(Date.now().toString());
     while(true){
       let current = array.find((element) => 
         Date.now() > element.nextDateTimeTriggered && element.frequence > 0
@@ -20,7 +20,7 @@ async function runTab(array) {
         )
       }
       console.log(current);
-      current.nextDateTimeTriggered = new Date(Date.now() + current.frequence*60*1000);
+      current.nextDateTimeTriggered = Date.now() + current.frequence*60*1000;
       tab.location = current.lien;
       await wait(current.duree*60*1000);
     }
